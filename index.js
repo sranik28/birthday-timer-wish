5e70f587347b7531a999fc5bb18fa3aa6a9b954c
sranik28~ <anikshariarrahman@gmail.com>
2023-03-19 02:29:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const endTime = '19 march 2023 2:45 AM';
+const time = document.querySelectorAll('.show-counter');
+console.log(time[0])
+const timercount = () =>{
+    const end = new Date(endTime);
+    const currentTime = new Date();
+    const deff = (end - currentTime) / 1000;
+    // console.log(deff)
+    if(end < currentTime){      
+        window.location.href = 'index2.html';
+        document.getElementById('myAudio').play();
+        return
+    }
+    time[0].innerText = Math.floor((deff / 3600) /24);
+    time[1].innerText = Math.floor((deff / 3600) % 60);
+    time[2].innerText = Math.floor((deff / 60) % 60);
+    time[3].innerHTML = Math.floor((deff) % 60); 
+    console.log(Math.floor((deff) % 60))
+}
+setInterval(() => {
+    timercount()
+    
+}, 1000);
---
+++ js.js
@@ -0,0 +1,3 @@
+document.getElementById('myAudio').play();
+document.getElementById('myAudio').pause()
+document.getElementById('myAudio').load()
